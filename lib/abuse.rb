f872fc02feabb79014f6cb2f2678fd450dc7c3f2
Dunya Kirkali <dunyakirkali@gmail.com>
2016-02-20 12:17:06 +0100

    Add confirmed and pending scopes to Abuse model
    Implement scopes for pending and confirmed abuses
    Boyscout

--- lib/abuse.rb
+++ lib/abuse.rb
@@ -1,6 +1,9 @@
 class Abuse < ActiveRecord::Base
-  belongs_to :resource, :polymorphic => true
+  belongs_to :resource, polymorphic: true
   
+  scope :pending, -> { where(confirmed: false) }
+  scope :confirmed, -> { where(confirmed: true) }
+
   def confirm!
     self.confirmed = true
     self.save unless self.new_record?
